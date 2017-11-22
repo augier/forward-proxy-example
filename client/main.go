2cc737ba72da0f4bde90be50ee1ceb707cc8c959
Chris Augier <chris.augier@gmail.com>
2017-11-22 09:50:49 +0000

    Adapting example for request response
    * Setting header data
    * using context

--- client/main.go
+++ client/main.go
@@ -15,7 +15,7 @@ func main() {
 		Transport: transport,
 	}
 	request, _ := http.NewRequest("GET", "http://localhost:8081", nil)
-
+	request.Header.Set("FOO", "BAR")
 	resp, _ := client.Do(request)
 	log.Println(resp.StatusCode)
 }
--- proxy/main.go
+++ proxy/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
 
 	"github.com/elazarl/goproxy"
@@ -11,9 +13,23 @@ func main() {
 	proxy.Verbose = true
 	proxy.OnRequest().DoFunc(
 		func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
-			r.Header.Set("X-GoProxy", "hello world")
+			log.Println(ctx.Session)
+			ctx.UserData = "hello"
+			c := r.Context()
+			c = context.WithValue(c, "bar", "foo")
+			r = r.WithContext(c)
 			return r, nil
 		})
+	proxy.OnResponse().DoFunc(
+		func(resp *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
+			r := ctx.Req
+			c := r.Context()
 
+			log.Println(ctx.Session)         // session cannot be used
+			log.Println(ctx.UserData)        // userdata can be used
+			log.Println(r.Header.Get("FOO")) // original request can be used
+			log.Println(c.Value("bar"))      // requests context can not be used
+			return resp
+		})
 	http.ListenAndServe(":8080", proxy)
 }
